c52818c410456906bd0d5a2bbd9116aa02182edf
Vinicius Brasil <vini@hey.com>
2022-07-08 12:40:43 -0300

    Initial commit

---
+++ lib/live_rss.ex
@@ -0,0 +1,18 @@
+defmodule LiveRSS do
+  @moduledoc """
+  Documentation for `LiveRSS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveRSS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/live_rss_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveRSSTest do
+  use ExUnit.Case
+  doctest LiveRSS
+
+  test "greets the world" do
+    assert LiveRSS.hello() == :world
+  end
+end
